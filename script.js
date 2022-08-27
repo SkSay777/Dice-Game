50249305eae6c5abe29829a64cfbb27882ebf77e
Sk Sayan <100615807+SkSay777@users.noreply.github.com>
2022-08-27 17:05:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,60 @@
-let h= Math.floor(Math.random() * 6) + 1;
-switch(h){
-    case 1:
-        document.querySelector(".img1").setAttribute("src","images/dice1.png");
-        break;
-    case 2:
-        document.querySelector(".img1").setAttribute("src","images/dice2.png");
-        break;
-    case 3:
-        document.querySelector(".img1").setAttribute("src","images/dice3.png");
-        break;
-    case 4:
-        document.querySelector(".img1").setAttribute("src","images/dice4.png");
-        break;
-    case 5:
-        document.querySelector(".img1").setAttribute("src","images/dice5.png");
-        break;
-    case 6:
-        document.querySelector(".img1").setAttribute("src","images/dice6.png");
-        break;
+
+function game() {
+    let h = Math.floor(Math.random() * 6) + 1;
+    switch (h) {
+        case 1:
+            document.querySelector(".img1").setAttribute("src", "images/dice1.png");
+            break;
+        case 2:
+            document.querySelector(".img1").setAttribute("src", "images/dice2.png");
+            break;
+        case 3:
+            document.querySelector(".img1").setAttribute("src", "images/dice3.png");
+            break;
+        case 4:
+            document.querySelector(".img1").setAttribute("src", "images/dice4.png");
+            break;
+        case 5:
+            document.querySelector(".img1").setAttribute("src", "images/dice5.png");
+            break;
+        case 6:
+            document.querySelector(".img1").setAttribute("src", "images/dice6.png");
+            break;
+    }
+    let g = Math.floor(Math.random() * 6) + 1;
+    switch (g) {
+        case 1:
+            document.querySelector(".img2").setAttribute("src", "images/dice1.png");
+            break;
+        case 2:
+            document.querySelector(".img2").setAttribute("src", "images/dice2.png");
+            break;
+        case 3:
+            document.querySelector(".img2").setAttribute("src", "images/dice3.png");
+            break;
+        case 4:
+            document.querySelector(".img2").setAttribute("src", "images/dice4.png");
+            break;
+        case 5:
+            document.querySelector(".img2").setAttribute("src", "images/dice5.png");
+            break;
+        case 6:
+            document.querySelector(".img2").setAttribute("src", "images/dice6.png");
+            break;
+    }
+    if (g > h) {
+        document.querySelectorAll("p")[1].innerHTML = "Player 2 🚩";
+    }
+    else {
+        document.querySelectorAll("p")[0].innerHTML = "Player 1 🚩";
+    }
 }
-let g= Math.floor(Math.random() * 6) + 1;
-switch(g){
-    case 1:
-        document.querySelector(".img2").setAttribute("src","images/dice1.png");
-        break;
-    case 2:
-        document.querySelector(".img2").setAttribute("src","images/dice2.png");
-        break;
-    case 3:
-        document.querySelector(".img2").setAttribute("src","images/dice3.png");
-        break;
-    case 4:
-        document.querySelector(".img2").setAttribute("src","images/dice4.png");
-        break;
-    case 5:
-        document.querySelector(".img2").setAttribute("src","images/dice5.png");
-        break;
-    case 6:
-        document.querySelector(".img2").setAttribute("src","images/dice6.png");
-        break;
-}
-if(g>h){
-    document.querySelector("div h1").innerHTML="Player 2 Wins";
-}
-else{
-    document.querySelector("div h1").innerHTML="Player 1 Wins";
-}
+   function rotate(){
+    document.querySelector(".img1").setAttribute("src", "images/dice.gif");
+    document.querySelector(".img2").setAttribute("src", "images/dice.gif");
+   } 
+game();
+let button = document.querySelector(".btn");
+    button.addEventListener('mousedown', function () {
+        rotate();
+    })
